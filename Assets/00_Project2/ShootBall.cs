235404bf51c7059f9bb60cb5fc10a5bed84df067
Jake <kubasmolka1999@gmail.com>
2021-03-21 23:56:07 +0100

    Project2-JakubSmolka

---
+++ Assets/00_Project2/ShootBall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ShootBall : MonoBehaviour
+{
+    Rigidbody ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            ball.AddForce(transform.right * 1000f);
+        }
+    }
+}
